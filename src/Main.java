e9a80537ed179b3c7fe12ba3f48f3cf0840900cc
Enes Akbulut <107497616+Enes-Akbulut@users.noreply.github.com>
2022-06-23 14:36:56 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+   //manav kasa programı
+
+        double armut =2.14, elma = 3.67, domates =1.11, muz =0.95, patlıcan =5, toplamTutar;
+        Scanner inp = new Scanner(System.in);
+        System.out.println("merhaba hosgeldin guzel abim, ne alırsın ");
+        System.out.print("kac kilo armut almak istersiniz?  ");
+        armut= inp.nextDouble();
+        System.out.print("kac kilo elma almak istersiniz?  ");
+        elma= inp.nextDouble();
+        System.out.print("kac kilo domates almak istersiniz?  ");
+        domates= inp.nextDouble();
+        System.out.print("kac kilo muz  almak istersiniz?  ");
+        muz= inp.nextDouble();
+        System.out.println("abi kusura bakma biraz zamlı patlıcanımız");
+        System.out.print("kac kilo patlıcan almak istersiniz?  ");
+        patlıcan= inp.nextDouble();
+        toplamTutar=armut+elma+domates+muz+patlıcan;
+        System.out.println("TOPLAM TUTAR = "+ toplamTutar);
+
+
+
     }
 }
